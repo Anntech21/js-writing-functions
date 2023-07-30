91aab31295256fcce0f947363e2e69d76310e650
Laura Hackney <laura@anniecannons.com>
2023-07-30 16:52:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Writing Functions in JavaScript
+
+## Activity 1
+1. Navigate to your Github profile page
+2. Identify as many different pieces of functionality that likely use functions. List them in this file.
+
+Functions here
+--
+
+--
+## Activity 2
+1. Connect your JavaScript file to your HTML file. Open your HTML file in the browser. 
+2. Read and follow the instructions for editing, writing, and calling functions.
---
+++ scripts.js
@@ -0,0 +1,63 @@
+// Activity 2
+
+
+/*********************** Exercise 1 ********************* */ 
+// Fix the following code blocks so that they will correctly run the code in the code block.
+
+var function=sayHello() {
+    alert("Hello");
+}
+
+function sayHello (
+    alert("Hello");
+)
+
+function sayHello {
+    alert("Hello");
+}
+
+/*********************** Exercise 2 ********************* */ 
+// Call the following function so that a random number is alerted in the browser.
+function alertRandom() {
+    let randomNumber = Math.floor(Math.random()*6)+1;
+    alert(randomNumber);
+};
+
+/*********************** Exercise 3 ********************* */ 
+
+// Create a function that prompts the user's for their favorite drink and logs the following phrase to the console. "My favorite drink is ________."
+
+// Execute the function so that you see "My favorite drink is iced coffee." in the console.
+
+/*********************** Exercise 4 ********************* */ 
+var user = {
+    id: 32,
+    fullName: 'Trinity Armstead',
+    languages: ['JavaScript', 'Python', 'Ruby']
+};
+
+// Create a function that creates an alert that says, "Welcome, Trinity!"
+
+// Execute the function
+
+/*********************** Exercise 5 ********************* */ 
+
+// Create a function that prompts the user for their birth year.
+// Use the following code to calculate the user's approximage age.
+    let todayDate = new Date();
+    let todayYear = todayDate.getFullYear();
+    let age = todayYear - birthYear;
+
+//Console log the sentence "Your approximate age is ____".
+
+// Execute the function
+
+/*********************** Exercise 6 ********************* */ 
+
+// Create a function that prompts the user for the following things: age, favoriteColor, favoriteMovie.
+
+// Create an object to store the user's responses. 
+
+// Log the following sentence to the console with the user's responses: "Your age is ____, your favorite color is ______, and your favorite movie is ______".
+
+// Execute the function
